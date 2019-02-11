77a21e4df6d47289e3ae54cbb123be7faf08afd2
Tomas Basham <tomasbasham@gmail.com>
2019-02-11 09:37:50 +0000

    Prepend the Google Cloud Storage URI to assets
    When the application is built for production each asset will be
    prepended with the URI to a Google Cloud Storage bucket. This URI will
    be the absolute path to the base asset directory where the application
    will be deployed.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      prepend: 'https://storage.googleapis.com/blunderlist/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
